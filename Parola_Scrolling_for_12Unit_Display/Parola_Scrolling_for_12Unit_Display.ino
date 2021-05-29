5e4cc680d9b1ed7ff03b5e96d37f32b8e7b0e892
Balveer Singh <balveersinghyt@gmail.com>
2021-05-29 22:43:39 +0530

    Add files via upload

---
+++ Parola_Scrolling_for_12Unit_Display/Parola_Scrolling_for_12Unit_Display.ino
@@ -0,0 +1,168 @@
+/*
+ * This product is made by Khalsa Education & Spirituality team.
+ * Designer:- Er. Ramandeep Singh
+ * Developer:- Er. Balveer Singh
+ * Contact Us:- 
+    *Email:- kesdevelopers@gmail.com  
+    *Website:- khalsaes.org
+ * All rights reserved © 2021 | May 30, 2021
+*/
+ 
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+//#include <SoftwareSerial.h>
+
+//SoftwareSerial Bluetooth(4,2); // RX, TX
+char indicator;
+char incomebyte;
+int count = 0;
+String message =""; 
+
+// set to 1 if we are implementing the user interface pot, switch, etc
+#define USE_UI_CONTROL 0
+
+#if USE_UI_CONTROL
+#include <MD_UISwitch.h>
+#endif
+
+// Turn on debug statements to the serial output
+#define DEBUG 0
+
+#if DEBUG
+#define PRINT(s, x) { Serial.print(F(s)); Serial.print(x); }
+#define PRINTS(x) Serial.print(F(x))
+#define PRINTX(x) Serial.println(x, HEX)
+#else
+#define PRINT(s, x)
+#define PRINTS(x)
+#define PRINTX(x)
+#endif
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 11
+#define CLK_PIN   13
+#define DATA_PIN  10
+#define CS_PIN    11
+
+// HARDWARE SPI
+//MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// SOFTWARE SPI
+MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+// Scrolling parameters
+#if USE_UI_CONTROL
+const uint8_t SPEED_IN = A5;
+const uint8_t DIRECTION_SET = 8;  // change the effect
+const uint8_t INVERT_SET = 0;     // change the invert
+
+const uint8_t SPEED_DEADBAND = 5;
+#endif // USE_UI_CONTROL
+
+uint8_t scrollSpeed = 35;    // default frame delay value
+textEffect_t scrollEffect = PA_SCROLL_LEFT;
+textPosition_t scrollAlign = PA_LEFT;
+uint16_t scrollPause = 0; // in milliseconds
+
+// Global message buffers shared by Serial and Scrolling functions
+#define	BUF_SIZE	50
+char curMessage[BUF_SIZE] = { " " };
+char newMessage[BUF_SIZE] = { "Wear Mask When You Enter In The Office" };
+bool newMessageAvailable = true;
+
+#if USE_UI_CONTROL
+
+MD_UISwitch_Digital uiDirection(DIRECTION_SET);
+MD_UISwitch_Digital uiInvert(INVERT_SET);
+
+void doUI(void)
+{
+  // set the speed if it has changed
+  {
+    int16_t speed = map(analogRead(SPEED_IN), 0, 1023, 10, 150);
+
+    if ((speed >= ((int16_t)P.getSpeed() + SPEED_DEADBAND)) ||
+      (speed <= ((int16_t)P.getSpeed() - SPEED_DEADBAND)))
+    {
+      P.setSpeed(speed);
+      scrollSpeed = speed;
+      PRINT("\nChanged speed to ", P.getSpeed());
+    }
+  }
+
+  if (uiDirection.read() == MD_UISwitch::KEY_PRESS) // SCROLL DIRECTION
+  {
+    PRINTS("\nChanging scroll direction");
+    scrollEffect = (scrollEffect == PA_SCROLL_LEFT ? PA_SCROLL_RIGHT : PA_SCROLL_LEFT);
+    P.setTextEffect(scrollEffect, scrollEffect);
+    P.displayClear();
+    P.displayReset();
+  }
+
+  if (uiInvert.read() == MD_UISwitch::KEY_PRESS)  // INVERT MODE
+  {
+    PRINTS("\nChanging invert mode");
+    P.setInvert(!P.getInvert());
+  }
+}
+#endif // USE_UI_CONTROL
+
+void readSerial(void)
+{
+  static char *cp = newMessage;
+  while (Serial.available())
+  {
+    *cp = (char)Serial.read();
+    Serial.println(cp);
+    
+    if ((*cp == '\n') || (cp - newMessage >= BUF_SIZE-2)) // end of message character or full buffer
+    {
+      *cp = '\0'; // end the string
+      // restart the index for next filling spree and flag we have a message waiting
+      cp = newMessage;
+      newMessageAvailable = true;
+    }
+    else  // move char pointer to next position
+      cp++;
+  }
+}
+
+void setup()
+{
+//  Bluetooth.begin(38400); // Default communication rate of the Bluetooth module
+  Serial.begin(9600);
+  //Serial.print("\n[Parola Scrolling Display]\nType a message for the scrolling display\nEnd message line with a newline");
+
+  #if USE_UI_CONTROL
+  uiDirection.begin();
+  uiInvert.begin();
+  pinMode(SPEED_IN, INPUT);
+
+  doUI();
+  #endif // USE_UI_CONTROL
+
+  P.begin();
+  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+}
+
+void loop()
+{
+  #if USE_UI_CONTROL
+  doUI();
+  Serial.println("Inside IF");
+  #endif // USE_UI_CONTROL
+  if (P.displayAnimate())
+  {
+    if (newMessageAvailable)
+    {
+      strcpy(curMessage, newMessage);
+      newMessageAvailable = false;
+    }
+    P.displayReset();
+  }
+  readSerial();
+ 
+}
